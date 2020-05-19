63769d694339702502c48050a5adf3a60fba968b
Guillaume <guillaume.martin.chomel@gmail.com>
2020-05-19 17:15:50 +0200

    creation d'un generateur aleatoire de requete d'api rawg (page découvrir

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { PageTestComponent } from './page-test/page-test.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { PageInscriptionComponent } from './page-inscription/page-inscription.component';
+import { PageArticleJeuComponent } from './page-article-jeu/page-article-jeu.component';
 
 
 const routes: Routes = [
   {path:'test', component: PageTestComponent},
   {path:'accueil', component: AccueilComponent},
   {path:'inscription', component: PageInscriptionComponent},
+  {path:'decouvrir', component: PageArticleJeuComponent},
   
 ];
 
--- src/app/page-article-jeu/page-article-jeu.component.ts
+++ src/app/page-article-jeu/page-article-jeu.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-page-article-jeu',
   templateUrl: './page-article-jeu.component.html',
   styleUrls: ['./page-article-jeu.component.scss']
 })
-export class PageArticleJeuComponent implements OnInit {
 
-  constructor() { }
+export class PageArticleJeuComponent implements OnInit {
+  nomJeu: String;
+  description;
+  image;
+  classements;
+  stores;
+  plateformes;
+
+  constructor( ) { }
 
   ngOnInit(): void {
+    var that=this;
+    var randomId=this.RandomIdGenerator(350000);
+
+    
+    // console.log("log on init randomId : "+randomId);
+    // console.log(this.URLConstructor(randomId));
+    $.get(this.URLConstructor(randomId),function(data){
+      console.log(data);
+      that.nomJeu=data.name;
+      that.description=data.description_raw;
+      that.image=data.background_image;
+      that.plateformes=data.plateforms;
+      that.stores=data.stores;
+    });  
+  }
+  URLConstructor(randomInt){
+    var URLapi="https://api.rawg.io/api";
+    var selector="/games";
+    var parameter="/"+250000;
+    var URLGenerated=(URLapi+selector+parameter);
+    console.log(URLGenerated);
+    return (URLGenerated)
+  }
+  RandomIdGenerator(max){
+    var randomInt=(Math.floor(Math.random() * Math.floor(max)));
+    console.log("log random int: "+randomInt)
+    return randomInt;
   }
 
 }
